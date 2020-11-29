6609304389333ea968edede7dff2d0a7581416bd
leonardossev <leonardossev@gmail.com>
2020-11-29 20:33:12 -0300

    fix(test): Imports the List interface

--- src/test/java/com/leonardossev/ecommerce/service/UserTypeServiceImplTest.java
+++ src/test/java/com/leonardossev/ecommerce/service/UserTypeServiceImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
